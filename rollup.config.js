d93a28a55b4e65be2ec6a9e618078d2a8f8afbd4
chanwit-y <chanwit.yimneam@hotmail.com>
2022-05-24 17:46:02 +0700

    config rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,48 @@
 import { babel } from "@rollup/plugin-babel";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-// import scss from "rollup-plugin-scss";
-import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import rollupNodeResolve from "rollup-plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import size from 'rollup-plugin-size';
+
+const packageJson = require("./package.json");
 
 export default [
   {
-    input: "./src/index.ts",
+    input: "src/index.ts",
     output: [
       {
-        file: "dist/index.js",
+        file: packageJson.main,
         format: "cjs",
+        sourcemap: true,
       },
       {
-        file: "dist/index.es.js",
-        format: "es",
-        exports: "named",
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
       },
     ],
     plugins: [
-      // scss({
-      //   output: true,
-      //   failOnError: true,
-      //   outputStyle: "compressed",
-      // }),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
       }),
       external(),
       resolve(),
-      typescript(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
+      rollupNodeResolve({
+        jsnext: true,
+        preferBuiltins: true,
+        browser: true
+      }),
+      json(),
+      size()
     ],
+    external: [ "react", "react-dom"],
   },
 ];
